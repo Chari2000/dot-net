c444385cd7f20c7f08a0c35dd6803a54a7027bdb
chari2000 <thipparthiuc143@gmail.com>
2022-01-31 10:16:27 +0530

    comments

---
+++ is and as/is and as/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace is_and_as
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            string s = "Demo class";
+            object obj = s;
+            //as operator
+            string s2 = obj as string;
+            //type casting
+            //boxing and unboxing
+            if(s2!=null)
+            {
+                Console.WriteLine("Successfully casted");
+                Console.WriteLine(s2);
+            }
+            
+        }
+    }
+}
