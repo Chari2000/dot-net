59adf0db94faa64149563d6d0a788df36c7a4765
chari2000 <thipparthiuc143@gmail.com>
2022-02-01 10:37:26 +0530

    comments

---
+++ SumOfNaturals/SumOfNaturals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfNaturals
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter how many natural numbers you want to add:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i < 10) 
+                { 
+                Console.Write(i + "+");
+                }
+                else 
+                {
+                    Console.Write(i);
+                }
+                sum += i;
+            }
+            Console.WriteLine("="+sum);
+        }
+    }
+}
---
+++ SumOfNaturals/SumOfNaturals/pattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNaturals
+{
+    class pattern
+    {
+        static void Main(string[] args)
+        {
+            int val = 4;
+            int i, j, k;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int range = 0;
+            for (i = 1; i <= val; i++)
+            {
+                for (j = 1; j <= val - i; j++)
+                {
+                    // Console.Write("");  
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(arr[range]+" ");
+                    range++;
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
