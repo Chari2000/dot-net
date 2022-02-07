d442c7925a9a3e967c5cd4ad8ffe0448a0a922c8
chari2000 <thipparthiuc143@gmail.com>
2022-02-07 15:44:22 +0530

    RegexHandling

---
+++ ConsoleApp2/ConsoleApp2/Generic2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Generic2
+    {
+
+        // we use < > to specify the parameter type
+        //Generic class
+
+        public class sample<T>
+        {
+            //field
+            private T data;
+
+            //property
+            public T value
+            {
+                //accessors
+                get
+                {
+                    return this.data;
+                }
+                set
+                {
+                    this.data = value;
+                }
+
+            }
+
+        }
+
+        public class Employee
+        {
+            public int EmpID { get; set; }
+            public string EmpName { get; set; }
+            public int EmpAge { get; set; }
+            public double EmpSal { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+            sample<string> name = new sample<string>();
+            name.value = "Chari";
+
+            sample<float> version = new sample<float>();
+            version.value = 5.0F;
+            //display Name
+            Console.WriteLine(name.value);
+            //display version
+            Console.WriteLine(version.value);
+
+            //Object Initialzier
+            Employee std = new Employee()
+            {
+                EmpID = 1,
+                EmpName = "Chari",
+                EmpAge = 27,
+                EmpSal = 200000
+            };
+            Console.WriteLine("Student Information");
+            Console.WriteLine(std.EmpID);
+            Console.WriteLine(std.EmpName);
+            Console.WriteLine(std.EmpAge);
+            Console.WriteLine(std.EmpSal);
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/ExceptionHandling3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class IntInString : Exception
+    {
+        public IntInString(string message) : base(message)
+        {
+
+        }
+    }
+    class ExceptionHandling3
+    {
+        string Character = Console.ReadLine();
+        string Special = "0123456789";
+        int c = 0;
+        public void TestChar()
+        {
+            for (int i = 0; i < Character.Length; i++)
+            {
+                for (int j = 0; j < Special.Length; j++)
+                {
+                    if (Character[i] == Special[j])
+                    {
+                        c = 1;
+                    }
+                }
+            }
+            if (c == 1)
+            {
+                throw (new IntInString("Integer found"));
+            }
+            else
+            {
+                Console.WriteLine("No integers are there");
+            }
+
+        }
+        static void Main3(string[] args)
+        {
+            ExceptionHandling3 Exp = new ExceptionHandling3();
+            try
+            {
+                Exp.TestChar();
+            }
+            catch (IntInString ex)
+            {
+                Console.WriteLine("Custom execption is {0}", ex.Message);
+            }
+        }
+    }
+}
