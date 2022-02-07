2034083eed2555f223de86ca64eda90a06c628f2
chari2000 <thipparthiuc143@gmail.com>
2022-02-07 10:30:21 +0530

    Generics

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Generics
+    {
+
+        public class sample<T>
+        {
+            private T data;
+            public T value
+            {
+                get
+                {
+                    return this.data;
+                }
+                set
+                {
+                    this.data = value;
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            sample<string> name = new sample<string>();
+            name.value = "Chari";
+
+            sample<float> version = new sample<float>();
+            version.value = 33.0F;
+            
+            Console.WriteLine(name.value);
+            
+            Console.WriteLine(version.value);
+        }
+    }
+}
