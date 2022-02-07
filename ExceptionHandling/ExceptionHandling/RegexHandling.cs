27af2092ae87057383cd828d7d053bc6a95e7c4e
chari2000 <thipparthiuc143@gmail.com>
2022-02-07 17:09:56 +0530

    Generics

--- ExceptionHandling/ExceptionHandling/RegexHandling.cs
+++ ExceptionHandling/ExceptionHandling/RegexHandling.cs
@@ -5,18 +5,25 @@ using System.Text.RegularExpressions;
 
 namespace ExceptionHandling
 {
-    internal class HandlingRegex
+    internal class RegexHandling
     {
         static void Main(string[] args)
         {
             //this will return any pattern line ab,aab,aaab;
             //Regex re = new Regex("a*b");
             //this will return pattern like b,ab
-            Regex re = new Regex("a?b");
-            Match match = re.Match("aaaaabcd");
+            //Regex re = new Regex("a?b");
+            Regex re = new Regex("[0-9]{4}$");
+
+            Match match = re.Match("ba12456");
             if (match.Success)
             {
                 Console.WriteLine("Mached the value " + match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid string");
+
             }
         }
     }
