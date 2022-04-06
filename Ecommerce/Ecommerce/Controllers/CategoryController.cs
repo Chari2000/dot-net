b96c0474349f7b9573a70ca640973ec336fcb158
chari2000 <thipparthiuc143@gmail.com>
2022-04-06 09:02:43 +0530

    Ecommerce

---
+++ Ecommerce/Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ecommerceContext _ecommerceDbContext;
+        public CategoryController(ecommerceContext ecommerceDbContext)
+        {
+            _ecommerceDbContext = ecommerceDbContext;
+        }
+        [HttpGet]
+        public IEnumerable<TblCategory> GetCategories()
+        {
+            return _ecommerceDbContext.TblCategories.ToList();
+        }
+    }
+}
---
+++ Ecommerce/Ecommerce/StartupBase.cs
@@ -0,0 +1,33 @@
+﻿namespace Ecommerce
+{
+    public class StartupBase
+    {
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public object Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwagger();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
